af3168acc932e2804d51429a64b431c285e8da31
BIGpony777 <BIGpony777@gmail.com>
2022-02-02 13:15:51 -0500

    Assignment 1 - I did it for like the third time

--- Assignment1/server.js
+++ Assignment1/server.js
@@ -51,37 +51,37 @@ function requestHandler(req, res){
 
 function dotted(query){
 
-  if(query['word1'] ==undefined || query['word2']==undefined){
+  if(query['word1'] == undefined || query['word2'] == undefined){
     throw Error('Expecting two words');   
   }
-  else if( query['word1'].length + query['word2'].length> 30){
+  else if( query['word1'].length + query['word2'].length > 30){
     throw Error('Words should be less than 30 characters in total');
   }
   else{
     let size = query['word1'].length + query['word2'].length;
-    let result= '';
+    let result = '';
     result += '<p> ';
     result += query['word1'];
-    for(let i=0; i<30 - size; i++){
+    for(let i = 0; i<30 - size; i++){
       result += '.';
     }
     result += ' ' + query['word2'];
-    result +=' </p>';
+    result += ' </p>';
     return result;
   }
 }
 
 function numCheck(query){
 
-  if(query['start'] ==undefined || query['end']==undefined ||query['start']>99||query['end']>99||query['start']>query['end']){
-    throw Error('Expecting a start and end number less than 99, where start is less than end');   
+  if(query['start'] == undefined || query['end'] == undefined){
+    throw Error('Expecting a start and end number');   
     }
   else{
     let start = query['start'];
-    let end = JSON.stringify(parseInt(query['end'])+1);
+    let end = JSON.stringify(parseInt(query['end']) + 1);
     let result = ' ';
     result += '<p>'      
-    for(let i =start; i<end; i++){
+    for(let i = start; i < end; i++){
       if (i % 3 == 0 && i % 5 == 0){
         result += 'ThFi';
   }else if(i % 3 == 0){
@@ -100,14 +100,14 @@ function numCheck(query){
 
 function gradeStats(query){
 
-  if (query['grade'] == undefined || query['grade'].length<2){
+  if (query['grade'] == undefined || query['grade'].length < 2){
     throw Error('You need to input at least two grades');
   }else{
     let size = query['grade'].length;
     let min = parseInt(query['grade'][0]);
     let max = parseInt(query['grade'][0]);
     let total = 0;
-    for (let i = 0;i < size;i++){
+    for (let i = 0; i < size;i++){
       total = total + parseInt(query['grade'][i]);
       if(min > parseInt(query['grade'][i])){
         min = parseInt(query['grade'][i]);
@@ -123,7 +123,7 @@ function gradeStats(query){
 
 function calculateRectangleProperties(query){
 
-  if (query['length']==undefined||query['width']==undefined){
+  if (query['length'] == undefined || query['width'] == undefined){
     throw Error('You need a length and width');
   }else{
     let length = parseInt(query['length']);
